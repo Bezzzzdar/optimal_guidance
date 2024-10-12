0824bb0311b2aca175c1b303501d7014c0a2f0e6
Bezzzzdar <ivan.rastegaev.03@mail.ru>
2024-10-12 23:49:32 +0300

    src/uav*: add model for uav

--- src/uav.cpp
+++ src/uav.cpp
@@ -1,9 +1,101 @@
 #include "UAV.h"
 
-UAV::UAV()
+void UAV::update_values(double t)
 {
+	this->v_values.push_back(this->state_vector->V);
+	this->theta_values.push_back(this->state_vector->theta);
+	this->phi_values.push_back(this->state_vector->phi);
+	this->x_values.push_back(this->state_vector->x);
+	this->y_values.push_back(this->state_vector->y);
+	this->z_values.push_back(this->state_vector->z);
+	
+	this->t_values.push_back(t);
+}
+
+UAV::UAV(state_vector_t *state_vector0, control_vector_t *u0, double t)
+{
+	this->state_vector = state_vector0;
+    this->u = u0;
+
+    this->update_values(t);
 }
 
 UAV::~UAV()
 {
+}
+
+void UAV::update_position(double t, double dt)
+{
+	this->state_vector->V += this->dv_dt() * dt;
+	this->state_vector->theta += this->dtheta_dt() * dt;
+	this->state_vector->phi += this->dphi_dt() * dt;
+	this->state_vector->x += this->dx_dt() * dt;
+	this->state_vector->y += this->dy_dt() * dt;
+	this->state_vector->z += this->dz_dt() * dt;
+	if (this->m < this->m0/2)
+	{
+		m = m0/2;
+		this->P = 0;
+	}
+	else
+	{
+		this->m += this->dm_dt() * dt;
+	}
+
+	this->update_values(t);
+	
+}
+
+void UAV::dump_values()
+{
+	std::fstream file;
+	file.open(this->output_file, std::ios::out | std::ios::app);
+	if (!(file.is_open()))
+	{
+		throw std::exception("File isn't open!");
+	}
+	std::cout << "file is open!" << std::endl;
+	for (size_t i = 0; i < this->x_values.size(); ++i) {
+		file << this->x_values[i] << "," << this->y_values[i] << "," << this->z_values[i] << "," << this->v_values[i] << "," << this->t_values[i] << "\n";
+	}
+	file.close();
+}
+
+/*
+* 	Basic system of equations of motion of the UAV as a material point
+*/
+
+double UAV::dv_dt()
+{
+	return g * (this->u->n_x - sin(this->state_vector->theta));
+}
+
+double UAV::dtheta_dt()
+{
+	return (g / this->state_vector->V) * (this->u->n_y - cos(this->state_vector->theta));
+}
+
+double UAV::dphi_dt()
+{
+	return -(g / this->state_vector->V) * (this->u->n_z / cos(this->state_vector->theta));
+}
+
+double UAV::dx_dt()
+{
+	return this->state_vector->V * cos(this->state_vector->theta) * cos(this->state_vector->phi);
+}
+
+double UAV::dy_dt()
+{
+	return this->state_vector->V * sin(this->state_vector->theta);
+}
+
+double UAV::dz_dt()
+{
+	return -(this->state_vector->V) * cos(this->state_vector->theta) * sin(this->state_vector->phi);
+}
+
+double UAV::dm_dt()
+{
+	return -(this->m_c);
 }
--- src/uav.h
+++ src/uav.h
@@ -1,13 +1,82 @@
 #pragma once
+#include <memory>
+#include <iostream>
+#include <array>
+#include <vector>
+#include <fstream>
+#include <cmath>
+#include <exception>
 
+constexpr double g = 9.81;
+
+/*******************************************************************
+*		
+*					Typedef for state vector
+* 
+*******************************************************************/
+typedef struct
+{
+	double V;
+	double theta;
+	double phi;
+	double x;
+	double y;
+	double z;
+} state_vector_t;
+
+/*******************************************************************
+*		
+*				Typedef for control vector by overload
+* 
+*******************************************************************/
+typedef struct
+{
+	double n_x;
+	double n_y;
+	double n_z;
+} control_vector_t;
+
+/*******************************************************************
+*
+*						Class for modeling uav  
+* 
+*******************************************************************/
 class UAV
 {
-public:
-	UAV();
-	~UAV();
-
-
 private:
+	double P = 40000;
+	double m = 10000;
+	double m0 = m;
+	double m_c = 200;
+	state_vector_t *state_vector;
+	control_vector_t *u;
 
+	std::vector<double> x_values;
+	std::vector<double> y_values;
+	std::vector<double> z_values;
+	std::vector<double> v_values;
+	std::vector<double> theta_values;
+	std::vector<double> phi_values;
+	std::vector<double> t_values;
+
+	const char* output_file = ".\\values\\uav_values.csv";
+
+	void update_values(double t);
+
+	/* Basic system of equations of motion of the UAV as a material point */
+	double dv_dt();
+	double dtheta_dt();
+	double dphi_dt();
+	double dx_dt();
+	double dy_dt();
+	double dz_dt();
+	double dm_dt();
+
+public:
+	UAV(state_vector_t *state_vector0, control_vector_t *u0, double t);
+	~UAV();
+	void update_position(double t, double dt);
+	void dump_values();
+	
 };
 
